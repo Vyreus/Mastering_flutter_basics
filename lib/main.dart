6df7c0624e23cf076606c5561c9ad397c4342231
Mahad Ali Siddiqui <mahadas2005@gmail.com>
2026-06-26 18:01:28 +0500

    1 commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body: Container(decoration: BoxDecoration(gradient: LinearGradient(
+
+colors: 
+
+        ),
+        ),          
+        child: const Center(
+            child: Text(
+              "Hello World"
+           )
+          ),
+        )
+      ),
+    ),
+  );
+}
+// new comment
