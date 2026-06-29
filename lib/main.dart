2b81c4a066c93f0701214a9f1f89e17b7a14cac3
Mahad Ali Siddiqui <mahadas2005@gmail.com>
2026-06-30 02:57:35 +0500

    Adding colors using boxdecoration in container widget and applying a gradient pattern.

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ void main() {
   runApp(
      MaterialApp(
       home: Scaffold(
-        body: Container(decoration: BoxDecoration(gradient: LinearGradient(
-
-colors: 
-
-        ),
+        body: Container(decoration:
+         const BoxDecoration(gradient:
+          LinearGradient(colors: [
+            Color.fromARGB(255, 10, 0, 65),
+            Color.fromARGB(255, 27, 1, 134), 
+            Color.fromARGB(255, 33, 2, 209),
+            ]
+         ),
         ),          
         child: const Center(
             child: Text(
