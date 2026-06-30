c7949f1b4d6ae2070c81eee13337f11d079aca52
Mahad Ali Siddiqui <mahadas2005@gmail.com>
2026-07-01 04:32:48 +0500

    Fixed the text rendering error!

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Container(decoration:
-         const BoxDecoration(gradient:
-          LinearGradient(colors: [
-            Color.fromARGB(255, 10, 0, 65),
-            Color.fromARGB(255, 27, 1, 134), 
-            Color.fromARGB(255, 33, 2, 209),
-            ]
-         ),
-        ),          
-        child: const Center(
-            child: Text(
-              "Hello World"
-           )
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 10, 0, 65),
+                Color.fromARGB(255, 27, 1, 134),
+                Color.fromARGB(255, 33, 2, 209),
+              ],
+            ),
           ),
-        )
+          child: const Center(
+            child: Text(
+              "Hello World",
+  
+            ),
+          ),
+        ),
       ),
     ),
   );
